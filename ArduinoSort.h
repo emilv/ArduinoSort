adcda4d4c0fcd21d26873fd921029349828f024c
Emil Vikström <emil@pixelstore.se>
2017-01-10 22:37:49 +0100

    Move implementation to header file

--- ArduinoSort.h
+++ ArduinoSort.h
@@ -15,7 +15,41 @@ template<typename AnyType> void sortArray(AnyType array[], size_t sizeOfArray, b
 // Sort in reverse with custom comparison function
 template<typename AnyType> void sortArrayReverse(AnyType array[], size_t sizeOfArray, bool (*largerThan)(AnyType, AnyType));
 
-// Create implementations for all types
-template<> void sortArray<int>(int*, size_t);
+/**** Implementation below ****/
+
+#define SWAP(x, y, T) do { T SWAP = x; x = y; y = SWAP; } while (0)
+
+template<typename AnyType> bool builtinLargerThan(AnyType first, AnyType second) {
+  return first > second;
+}
+
+template<> bool builtinLargerThan(char* first, char* second) {
+  return strcmp(first, second) > 0;
+}
+
+template<typename AnyType> void insertionSort(AnyType array[], size_t sizeOfArray, bool reverse, bool (*largerThan)(AnyType, AnyType)) {
+  for (size_t i = 1; i < sizeOfArray; i++) {
+    for (size_t j = i; j > 0 && (largerThan(array[j-1], array[j]) != reverse); j--) {
+      SWAP(array[j-1], array[j], AnyType);
+    }
+  }
+}
+
+template<typename AnyType> void sortArray(AnyType array[], size_t sizeOfArray) {
+	insertionSort(array, sizeOfArray, false, builtinLargerThan);
+}
+
+template<typename AnyType> void sortArrayReverse(AnyType array[], size_t sizeOfArray) {
+	insertionSort(array, sizeOfArray, true, builtinLargerThan);
+}
+
+template<typename AnyType> void sortArray(AnyType array[], size_t sizeOfArray, bool (*largerThan)(AnyType, AnyType)) {
+	insertionSort(array, sizeOfArray, false, largerThan);
+}
+
+template<typename AnyType> void sortArrayReverse(AnyType array[], size_t sizeOfArray, bool (*largerThan)(AnyType, AnyType)) {
+	insertionSort(array, sizeOfArray, true, largerThan);
+}
+
 
 #endif
